605840536e07415590a02eacf6f1163cffeb27e7
yuanchuan <yuanchuan23@gmail.com>
2014-09-01 18:31:07 +0800

    generator for dragon-curve

---
+++ src/dragon-curve/generator.hs
@@ -0,0 +1,34 @@
+import System.Environment
+import Data.List 
+
+cell :: [Char] -> [Char]
+cell seed =  intercalate seed ["<div>", "</div>"]  
+
+build :: Int -> ([Char] -> [Char]) -> [Char]
+build level generator = apply level (cell "") generator
+    where 
+      apply level seed generator
+          | level <= 0 =  seed
+          | otherwise  =  apply (level - 1) (generator seed) generator 
+
+
+-- | base structure
+-- 
+--  <div>
+--    <div></div>
+--    <div></div>
+
+--    <div></div>
+--  </div>
+--
+generator :: [Char] -> [Char]    
+generator seed = cell (intercalate "" [(cell seed), (cell seed), (cell "")])
+
+
+main :: IO()
+main = do
+    args <- getArgs
+    putStrLn (build (getLevel args) generator)  
+        where 
+          getLevel []   = 0
+          getLevel args = read (head args) :: Int
