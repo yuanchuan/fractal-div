0c2ef5a5655439554867b250c49f1b4e442065a9
yuanchuan <yuanchuan23@gmail.com>
2014-09-10 22:58:53 +0800

    add golden spiral

---
+++ src/golden-spiral/generator.hs
@@ -0,0 +1,34 @@
+import System.Environment
+import Data.List 
+
+cell :: [Char] -> [Char]
+cell seed =  intercalate seed ["<div>", "</div>"]  
+
+build :: Int -> ([Char] -> [Char]) -> [Char]
+build order generator = apply order (cell "") generator
+    where 
+      apply order seed generator
+          | order <= 0 =  seed
+          | otherwise  =  apply (order - 1) (generator seed) generator 
+
+
+-- | base structure
+-- 
+--  <div>
+--    <div></div>
+--    <div>
+--      <div></div>
+--    </div>
+--  </div>
+--
+generator :: [Char] -> [Char]    
+generator seed = cell ((cell "") ++ seed)
+
+
+main :: IO()
+main = do
+    args <- getArgs
+    putStrLn (build (getLevel args) generator)  
+        where 
+          getLevel []   = 0
+          getLevel args = read (head args) :: Int
